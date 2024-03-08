a7a2f212308e5f6838d692d7329b15bcb00897d8
JANARTHANAN B <95681443+JANARTHANAN13102002@users.noreply.github.com>
2024-03-08 10:00:52 +0000

    Msg

---
+++ JS/W3Schools/events.js
@@ -0,0 +1,25 @@
+// OnChange
+
+    function myfunction()
+    {
+        let x=document.getElementById('id1').value;
+        document.getElementById('demo').innerHTML="You Selected : "+x;
+    }
+
+
+    function bigimg(x)
+    {
+        x.style.width="100px";
+        x.style.height="100px";
+    }
+    function norimg(x)
+    {
+        x.style.width="50px";
+        x.style.height="50px";
+    }
+
+
+    function Myfunction() {
+        let x = document.getElementById('2').value;
+        document.getElementById('run').innerHTML = "You have Typed : "+x;
+    }
---
+++ JS/W3Schools/function.js
@@ -0,0 +1,8 @@
+
+    function myFunction(p1,p2) {
+        return  p1*p2;
+    }
+
+    let result = myFunction(4,52);
+    document.getElementById('demo').innerHTML=result;
+    
